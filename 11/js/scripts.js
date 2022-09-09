0c69bf3785920ab15bcf2a5e2b6c071f693ff91c
Keks <keks@htmlacademy.ru>
2022-09-10 00:12:35 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/scripts.js
+++ 11/js/scripts.js
@@ -1,7 +1,1 @@
-const button = document.querySelector('.main-nav__button'); // находим кнопку
-const nav = document.querySelector('.main-nav'); // находим навигацию
-const closedClass = 'main-nav--closed'; // класс, который будем добавлять или удалять с навигации
-
-button.addEventListener('click', () => { // слушаем клике на кнопке
-  nav.classList.toggle(closedClass); // Как только на кнопке произойдёт клик у навигации смениться класс main-nav--closed
-});
+const button=document.querySelector(".main-nav__button"),nav=document.querySelector(".main-nav"),closedClass="main-nav--closed";button.addEventListener("click",(()=>{nav.classList.toggle(closedClass)}));
